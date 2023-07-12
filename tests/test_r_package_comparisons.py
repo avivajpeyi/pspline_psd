c212fb7e6556774acb6d871123f1c800551b3d55
avi <avi.vajpeyi@gmail.com>
2023-07-12 14:31:02 +1200

    add more tests

--- tests/test_r_package_comparisons.py
+++ tests/test_r_package_comparisons.py
@@ -26,21 +26,99 @@ except ImportError:
 np.random.seed(0)
 @pytest.mark.skipif(rpy2 is None, reason="rpy2 required for this test")
 def test_direct_comparison(helpers):
+    """Check that the r package and py package have the same
+    - fz
+    - db_list
+    - tau_vals
+    - lnl
+    - psd
+
+    """
     data = helpers.load_raw_data()
     n = len(data)
     omega = np.linspace(0, 1, n // 2 + 1)
     degree = 3
     k = 32
 
-    r_data = __compute_r_psd(data, k, degree, omega)
-    py_data = __compute_py_psd(data, k, degree, omega)
+    r_data = __collect_r_psline_data(data, k, degree, omega)
+    py_data = __collect_py_pspline_data(data, k, degree, omega)
 
     fig = __make_comparison_plot(r_data, py_data)
     fig.savefig(f"{helpers.OUTDIR}/r_package_comparison.png")
-    # plt.show()
+    plt.show()
 
 
-def __compute_r_psd(data, k, degree, omega):
+    assert np.allclose(r_data["dblist"], py_data["dblist"])
+    assert np.allclose(r_data["tau_vals"], py_data["tau_vals"])
+    assert np.allclose(r_data["psd"], py_data["psd"], atol=0.025)
+
+    rel_lnl_diff = (r_data["lnl"]-py_data["lnl"])/py_data["lnl"]
+    assert np.allclose(rel_lnl_diff, 0, atol=0.5)
+
+    # The following will fail:
+    # assert np.allclose(r_data["psd"], py_data["psd"], atol=0.001)
+    # assert np.allclose(r_data["fz"], py_data["fz"])
+
+
+
+
+
+@pytest.mark.skipif(rpy2 is None, reason="rpy2 required for this test")
+def test_psd_postproc(helpers):
+    data = helpers.load_raw_data()
+    nsteps = 1200
+    r_data = __r_mcmc(data, nsteps)
+    py_data = __py_mcmc(data, nsteps)
+    omega = np.linspace(0, np.pi, len(r_data.psd[:, 0]))
+    py_post = generate_psd_quantiles(omega, r_data.dblist.T, r_data.samples[:, 2], r_data.v.T)
+
+    plt.rcParams['hatch.linewidth'] = 2.0
+    plt.fill_between(omega, py_data.psd_quantiles[1, :], py_data.psd_quantiles[2, :], alpha=0.3)
+    plt.fill_between(omega, py_post[1, :], py_post[2, :], alpha=0.3,  lw=3, hatch="/", edgecolor='tab:orange', color='tab:orange')
+    plt.fill_between(omega, r_data.psd_quantiles[1, :], r_data.psd_quantiles[2, :], alpha=0.3, hatch="\\", edgecolor='tab:green', color='tab:green')
+    plt.legend(['Python', 'Python Post', 'R'])
+    plt.xlim(omega[1], omega[-2])
+    plt.ylim(0, 3)
+    plt.xlabel('Freq')
+    plt.ylabel('PSD')
+    plt.show()
+
+
+@pytest.mark.skipif(rpy2 is None, reason="rpy2 required for this test")
+def test_mcmc_comparison(helpers):
+    nsteps = 100
+    data = helpers.load_raw_data()
+    r_mcmc = __r_mcmc(data, nsteps)
+    py_mcmc = __py_mcmc(data, nsteps)
+    n, newn = len(data), len(py_mcmc.psd_quantiles[0])
+    periodogram = np.abs(np.power(fft(data), 2) / (2 * np.pi * n))[0:newn]
+    psd_x = np.arange(0, newn)
+    plt.style.use(
+        'https://gist.githubusercontent.com/avivajpeyi/4d9839b1ceb7d3651cbb469bc6b0d69b/raw/4ee4a870126653d542572372ff3eee4e89abcab0/publication.mplstyle')
+    plt.rcParams['font.family'] = 'sans-serif'
+    plt.plot(figsize=(8, 4))
+    plt.scatter(psd_x, periodogram, color='k', label='Data', s=0.75)
+    plt.plot(psd_x, py_mcmc.psd_quantiles[0], color='tab:orange', alpha=0.5, label='Python (Uniform CI)')
+    plt.fill_between(psd_x, py_mcmc.psd_quantiles[1], py_mcmc.psd_quantiles[2], color='tab:orange', alpha=0.2, linewidth=0.0)
+    plt.plot(psd_x, r_mcmc.psd_quantiles[0], color='tab:green', alpha=0.5, label='R (Uniform CI)')
+    plt.fill_between(psd_x, r_mcmc.psd_quantiles[1], r_mcmc.psd_quantiles[2], color='tab:green', alpha=0.2, linewidth=0.0)
+    # turn off grid
+    plt.grid(False)
+    # set font to sans-serif
+    # legend but increase size of markers
+    plt.legend(markerscale=5, frameon=False)
+    plt.ylabel('PSD')
+    plt.xlabel('IDX')
+    plt.tight_layout()
+    # turn off minor ticks
+    plt.minorticks_off()
+    plt.savefig(f'{helpers.OUTDIR}/psd_comparison.png', dpi=300)
+    plt.show()
+
+
+
+
+def __collect_r_psline_data(data, k, degree, omega):
     r_pspline = importr("psplinePsd")
     np_cv_rules = default_converter + numpy2ri.converter
 
@@ -60,11 +138,11 @@ def __compute_r_psd(data, k, degree, omega):
     return {'fz': fz, 'v': v, 'dblist': dblist, 'psd': psd, 'tau_vals': tau_vals, 'lnl': lnl}
 
 
-def __compute_py_psd(data, k, degree, omega):
+def __collect_py_pspline_data(data, k, degree, omega):
     fz = get_fz(data)
     pdgrm = np.power(np.abs(fz), 2)
     v = _generate_initial_weights(pdgrm, k)
-    knots = knot_locator(data=data, k=k, degree=degree, eqSpaced=True)
+    knots = knot_locator(pdgrm=pdgrm, k=k, degree=degree, eqSpaced=True)
     dblist = dbspline(x=omega, knots=knots, degree=degree)
     psd = get_unormalised_psd(v, dblist)
 
@@ -77,7 +155,7 @@ def __compute_py_psd(data, k, degree, omega):
 
 
 def __make_comparison_plot(r_data, py_data):
-    fig, axes = plt.subplots(5, 1, figsize=(10, 10))
+    fig, axes = plt.subplots(7, 1, figsize=(6, 12))
     rkwgs = dict(color='tab:red', alpha=0.5, lw=4, zorder=-10, label='r')
     pykwgs = dict(color='tab:blue', alpha=1, ls='--', lw=2, label='py')
     difkwgs = dict(color='tab:green', alpha=1, lw=2, label='diff')
@@ -105,49 +183,22 @@ def __make_comparison_plot(r_data, py_data):
     rel_diff = np.abs(r_data['psd'] - py_data['psd']) / np.abs(r_data['psd'])
     axes[3].plot(rel_diff[1:-3], **difkwgs)
     axes[3].set_ylabel(r'$\frac{|S_{\rm r} - S_{\rm py} |}{|S_{r}|}$')
+    axes[4].hist(rel_diff[1:-3], density=True, color='tab:green')
+    axes[4].set_xlabel('Hist of relative difference bw psd')
 
     # plot lnl
     # relative difference bw lnl
     rel_diff = np.abs(r_data['lnl'] - py_data['lnl']) / np.abs(r_data['lnl'])
-    axes[4].plot(r_data['tau_vals'], rel_diff, **difkwgs)
+    axes[5].plot(r_data['tau_vals'], rel_diff, **difkwgs)
     # axes[4].plot(py_data['tau_vals'], py_data['lnl'], **pykwgs)
-    axes[4].set_ylabel(r'$\frac{|lnl_{\rm r} - lnl_{\rm py} |}{|lnl_{r}|}$')
-    axes[4].set_xlabel('tau')
-    axes[4].set_xscale('log')
+    axes[5].set_ylabel(r'$\frac{|lnl_{\rm r} - lnl_{\rm py} |}{|lnl_{r}|}$')
+    axes[5].set_xlabel('tau')
+    axes[5].set_xscale('log')
+    axes[6].hist(rel_diff, density=True,  color='tab:green')
+    axes[6].set_xlabel('Hist of relative difference bw lnl')
 
     plt.tight_layout()
     return fig
-
-
-@pytest.mark.skipif(rpy2 is None, reason="rpy2 required for this test")
-def test_mcmc_comparison(helpers):
-    nsteps = 100
-    data = helpers.load_raw_data()
-    r_psd, r_psd_p05, r_psd_p95 = __r_mcmc(data, nsteps)
-    py_psd, py_psd_p05, py_psd_p95 = __py_mcmc(data, nsteps)
-    n, newn = len(data), len(py_psd)
-    periodogram = np.abs(np.power(fft(data), 2) / (2 * np.pi * n))[0:newn]
-    psd_x = np.linspace(0, 3.14, newn)
-    plt.style.use(
-        'https://gist.githubusercontent.com/avivajpeyi/4d9839b1ceb7d3651cbb469bc6b0d69b/raw/4ee4a870126653d542572372ff3eee4e89abcab0/publication.mplstyle')
-    plt.rcParams['font.family'] = 'sans-serif'
-    plt.plot(figsize=(8, 4))
-    plt.scatter(psd_x[1:], periodogram[1:], color='k', label='Data', s=0.75)
-    plt.plot(psd_x[1:], py_psd[1:], color='tab:orange', alpha=0.5, label='Python (Uniform CI)')
-    plt.fill_between(psd_x[1:], py_psd_p05[1:], py_psd_p95[1:], color='tab:orange', alpha=0.2, linewidth=0.0)
-    plt.plot(psd_x[1:], r_psd[1:], color='tab:green', alpha=0.5, label='R (Uniform CI)')
-    plt.fill_between(psd_x[1:], r_psd_p05[1:], r_psd_p95[1:], color='tab:green', alpha=0.2, linewidth=0.0)
-    # turn off grid
-    plt.grid(False)
-    # set font to sans-serif
-    # legend but increase size of markers
-    plt.legend(markerscale=5, frameon=False)
-    plt.ylabel('PSD')
-    plt.xlabel('Freq')
-    plt.tight_layout()
-    # turn off minor ticks
-    plt.minorticks_off()
-    plt.savefig(f'{helpers.OUTDIR}/psd_comparison.png', dpi=300)
 
 
 def __r_mcmc(data, nsteps=200):
@@ -200,21 +251,3 @@ class MCMCdata:
         return obj
 
 
-def test_psd_postproc(helpers):
-    data = helpers.load_raw_data()
-    nsteps = 1200
-    r_data = __r_mcmc(data, nsteps)
-    py_data = __py_mcmc(data, nsteps)
-    omega = np.linspace(0, np.pi, len(r_data.psd[:, 0]))
-    py_post = generate_psd_quantiles(omega, r_data.dblist.T, r_data.samples[:, 2], r_data.v.T)
-
-    plt.rcParams['hatch.linewidth'] = 2.0
-    plt.fill_between(omega, py_data.psd_quantiles[1, :], py_data.psd_quantiles[2, :], alpha=0.3)
-    plt.fill_between(omega, py_post[1, :], py_post[2, :], alpha=0.3,  lw=3, hatch="/", edgecolor='tab:orange', color='tab:orange')
-    plt.fill_between(omega, r_data.psd_quantiles[1, :], r_data.psd_quantiles[2, :], alpha=0.3, hatch="\\", edgecolor='tab:green', color='tab:green')
-    plt.legend(['Python', 'Python Post', 'R'])
-    plt.xlim(omega[1], omega[-2])
-    plt.ylim(0, 3)
-    plt.xlabel('Freq')
-    plt.ylabel('PSD')
-    plt.show()
